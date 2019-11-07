0a172b168970def0f56d2efee47eb782737f4ceb
Victor Neerholt <victorneerholt@gmail.com>
2019-11-07 13:29:20 +0100

    Lavet noget css

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php echo 'Test'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Indtast information</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="container"><!--Container-->
+        <div id="header-container"><!--Header-Container-->
+            <div id="header"><!--header-->
+                <center><h1>Test</h1></center>  
+            </div><!--header-->
+            <div id="nav-bar"><!--navbar-->
+                <ul>
+                    <li class="selected"><a href="index.php">Forside</a></li>
+                    <li><a href="pro.php">Projekter</a></li>
+                    <li><a href="kon.php">Kontakt</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li style="float:right"><a href="https://steamcommunity.com/id/qvikke93steamaccount" target="_blank">Steam</a></li>
+                    <li style="float:right"><a href="#">Facebook</a></li>
+                    <li style="float:right"><a href="https://twitter.com/qvikke93" target="_blank">Twitter</a></li>
+                    <li style="float:right"><a href="#">Linkedin</a></li>
+                    <li style="float:right"><a href="#">Praktikpladsen</a></li>
+                </ul> 
+            </div><!--navbar-->
+        </div><!--Header-Container-->
+        <div id="main-container"><!--main-container-->
+                <center><h1>Test</h1></center>
+      </div><!--main-container-->
+      <div id="footer"><!--footer-->
+          <p>Website made by Victor Neerholt</p> 
+      </div><!--footer-->
+    </div><!--Container-->
+</body>
+</html>
